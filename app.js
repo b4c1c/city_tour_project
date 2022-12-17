b6021affd510a4bf6b3a8857956368e17a59a0cc
b4c1c <b4c1c@b4c1c-device.local>
2022-12-17 11:18:25 +0100

    Created project

---
+++ app.js
@@ -0,0 +1,10 @@
+// Line 1 - 5 is to test JS file. Running fine 
+
+// let about = document.getElementById('about')
+// about.style.color = 'red'
+// console.log(about);
+
+function darkModeFunction(){
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+}
