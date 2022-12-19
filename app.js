6d4d1bbac8f4b0cba35398450c18d462c37ff5cf
b4c1c <b4c1c@b4c1c-device.local>
2022-12-19 09:59:47 +0100

    Small redesign

--- app.js
+++ app.js
@@ -4,7 +4,21 @@
 // about.style.color = 'red'
 // console.log(about);
 
-function darkModeFunction(){
-    let element = document.body;
-    element.classList.toggle("dark-mode");
+let icon = document.getElementById("icon")
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")){
+        icon.src = "images/sun.png";        
+    } else {
+        icon.src = "images/moon.png"
+    }
 }
+
+
+
+// function darkModeFunction(){
+//     let element = document.body;
+//     element.classList.toggle("dark-mode");
+// }
+
